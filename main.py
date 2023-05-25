6c6e9650f8d79543adae00af1fc2e106039953d3
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2023-05-25 17:07:43 +0000

    Add title and description scenes.

---
+++ main.py
@@ -0,0 +1,29 @@
+from manim import *
+
+def FadeOutAll(scene : Scene):
+    scene.play(*[FadeOut(i) for i in scene.mobjects])
+
+class Stat_Title(Scene):
+    def construct(self):
+        title = Text ("CS Camp Pilot Survey Statistics")
+        self.play(Write(title))
+        self.wait(1)
+        FadeOutAll(self)
+
+class Description(Scene):
+    def construct(self):
+        title = Title ("Description")
+        self.play(Write(title))
+        description = VGroup(
+            Text ("""
+Is there an association between grade level and coding experience 
+for primary and secondary school students?""", font_size=24, slant=ITALIC), 
+            BulletedList (
+                r"$H_0$: There is $\bold{no}$ association between grade level and coding experience.", 
+                r"$H_a$: There is $\bold{an}$ association between grade level and coding experience.", 
+                font_size=36
+            ).move_to(2 * DOWN)
+        ).next_to(title, 2 * DOWN)
+        self.play(Write(description))
+        self.wait(1)
+        FadeOutAll(self)
