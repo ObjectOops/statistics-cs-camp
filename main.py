cbfc138b946cf51a0ae9c71c20e241993accd291
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2023-05-25 23:11:58 +0000

    Update description.

--- main.py
+++ main.py
@@ -17,12 +17,14 @@ class Description(Scene):
         description = VGroup(
             Text ("""
 Is there an association between grade level and coding experience 
-for primary and secondary school students?""", font_size=24, slant=ITALIC), 
+for intermediate grade school students?""", font_size=24, slant=ITALIC), 
             BulletedList (
                 r"$H_0$: There is $\bold{no}$ association between grade level and coding experience.", 
                 r"$H_a$: There is $\bold{an}$ association between grade level and coding experience.", 
                 font_size=36
-            ).move_to(2 * DOWN)
+            ).move_to(2 * DOWN), 
+            Text ("(For intermediate grade school students.)", 
+                font_size=16, slant=ITALIC).move_to(3.5 * DOWN)
         ).next_to(title, 2 * DOWN)
         self.play(Write(description))
         self.wait(1)
